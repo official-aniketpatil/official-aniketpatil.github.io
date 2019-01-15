b7a9b6fad03ac6b20a527bb59041ffe10b5f6e7a
Aniket Patil <aniketpatil32@gmail.com>
2019-01-15 18:23:44 +0530

    Add files via upload
    initiated

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var myImage=document.querySelector('img');
+myImage.onclick=function(){
+	var mySrc=myImage.getAttribute('src');
+	if(mySrc==='images/profile.jpg'){
+		myImage.setAttribute('src','images/profile1.jpg');
+	}else{
+		myImage.setAttribute('src','images/profile.jpg');
+	}
+}
+var myButton=document.querySelector('button');
+var myHeading=document.querySelector('h1');
+
+function setUserName(){
+	var myName=prompt('please enter your name');
+	localStorage.setItem('name',myName);
+	myHeading.textContent="Welcome "+myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome, ' + storedName;
+}
+myButton.onclick=function(){
+	setUserName();
+}
