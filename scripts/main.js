f63eb99ddb4affe0b739c55453e1b6ecb6dd8131
aniket patil <aniketpatil32@gmail.com>
2019-01-15 18:55:29 +0530

    Update main.js
    home edited

--- scripts/main.js
+++ scripts/main.js
@@ -7,20 +7,4 @@ myImage.onclick=function(){
 		myImage.setAttribute('src','images/profile.jpg');
 	}
 }
-var myButton=document.querySelector('button');
 var myHeading=document.querySelector('h1');
-
-function setUserName(){
-	var myName=prompt('please enter your name');
-	localStorage.setItem('name',myName);
-	myHeading.textContent="Welcome "+myName;
-}
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Welcome, ' + storedName;
-}
-myButton.onclick=function(){
-	setUserName();
-}
